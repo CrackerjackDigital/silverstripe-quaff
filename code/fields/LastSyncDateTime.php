1b822ae143c0533b2b071e3bbf98b754016789de
Steve Wake <steve@moveforward.co.nz>
2016-10-20 13:00:00 +1300

    don't require LastSyncDateTime

--- code/fields/LastSyncDateTime.php
+++ code/fields/LastSyncDateTime.php
@@ -5,6 +5,9 @@ class LastSyncDateTime extends \Modular\Fields\DateTimeField {
 	const SingleFieldName = 'QuaffLastSyncDateTime';
 	const SingleFieldSchema = 'SS_DateTime';
 
+	const DateRequired = false;
+	const TimeRequired = false;
+
 	public function afterQuaff() {
 		$this()->{static::field_name()} = \SS_Datetime::now()->Rfc2822();
 	}
