5e41da7ba1d32db18b97e329ee3575b3566b8f98
Steve Wake <steve@moveforward.co.nz>
2017-06-14 16:30:01 +1200

    tidy logging

--- code/Api.php
+++ code/Api.php
@@ -147,7 +147,6 @@ abstract class Api extends Object
 		// now get the endpoints to sync themselves
 		/** @var EndpointInterface $endpoint */
 		foreach ($endpoints as $endpointPath => $endpoint) {
-
 			$endpoint->sync();
 		}
 		if (!Director::is_cli()) {
@@ -196,7 +195,9 @@ abstract class Api extends Object
 	 * Return a configured endpoint.
 	 *
 	 * @param string $path
-	 * @return EndpointInterface
+	 *
+	 * @return \Quaff\Interfaces\Endpoint
+	 * @throws \Quaff\Exceptions\Exception
 	 */
 	public static function endpoint($path) {
 		foreach (static::endpoints() as $testPath => $config) {
@@ -275,7 +276,9 @@ abstract class Api extends Object
 	 * @param array $config
 	 *
 	 * @param bool  $dereferenceBase
+	 *
 	 * @return array
+	 * @throws \Quaff\Exceptions\Exception
 	 */
 	protected static function decode_config(array $config, $dereferenceBase = false) {
 		$merged = [
